9130ec6b2c1adb6f24e76930dfdd624efc8943c2
Raphaël PERRIN <raphael_perrin@Ordinateur-portable-de-Raphael.local>
2024-07-04 23:14:06 +0200

    feat: menu bar & beginning home section

---
+++ src/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+
+import './Menu.css';
+import language from '../Images/anglais.png';
+
+export default function Menu() {
+    return <div className={"Menu"}>
+        <p>Accueil</p>
+        <p>Portfolio</p>
+        <p>A propos de moi</p>
+        <p>Contact</p>
+        <img src={language} alt="language" width={"2.5%"}/>
+    </div>;
+}
